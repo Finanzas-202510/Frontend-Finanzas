65c37c4b1b8561b0c648b63d9013fc66c0c603ac
Yair Aru Acevedo <u202125984@upc.edu.pe>
2025-06-30 22:39:50 -0500

    feat: add guards for user in login and dashboard.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,15 @@ import { BonosVistaComponent } from './Bonos/bonos-vista.component/bonos-vista.c
 import { BonoEditarComponent} from './Bonos/bonos-editar.component/bonos-editar.component';
 import {LoginComponent} from './Auth/pages/login.component/login.component';
 import {RegisterComponent} from './Auth/pages/register.component/register.component';
+import {LoginGuard} from './Auth/services/login-guard';
+import {AuthGuard} from './Auth/services/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path:'login', component: LoginComponent},
-  { path:'register', component: RegisterComponent},
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'nuevo-bono', component: NuevoBonoComponent },
-  { path: 'vista-bono/:id', component: BonosVistaComponent },
-  { path: 'vista-bono/editar/:id', component: BonoEditarComponent },
+  { path:'login', component: LoginComponent, canActivate: [LoginGuard] },
+  { path:'register', component: RegisterComponent, canActivate: [LoginGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'nuevo-bono', component: NuevoBonoComponent , canActivate: [AuthGuard] },
+  { path: 'vista-bono/:id', component: BonosVistaComponent , canActivate: [AuthGuard] },
+  { path: 'vista-bono/editar/:id', component: BonoEditarComponent , canActivate: [AuthGuard] },
 ];
