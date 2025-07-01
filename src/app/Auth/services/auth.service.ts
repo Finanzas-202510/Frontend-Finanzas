8e96307dfab42be035cc379932266ff722d2b692
Yair Aru Acevedo <u202125984@upc.edu.pe>
2025-06-30 21:57:31 -0500

    feat: add localstorage about user information.

--- src/app/Auth/services/auth.service.ts
+++ src/app/Auth/services/auth.service.ts
@@ -12,6 +12,19 @@ export class AuthService {
   }
   login(email: string, password: string): Observable<boolean> {
     return this.http.get<any[]>(`${this.apiUrl}?email=${email}&password=${password}`)
-      .pipe(map(users => users.length > 0));
+      .pipe(
+        map(users => {
+          if (users.length > 0) {
+            localStorage.setItem('user', JSON.stringify(users[0]));
+            return true;
+          }
+          return false;
+        })
+      );
   }
+  getCurrentUser(): any | null {
+    const userJson = localStorage.getItem('user');
+    return userJson ? JSON.parse(userJson) : null;
+  }
+
 }
--- src/app/Shared/toolbar.component/toolbar.component.ts
+++ src/app/Shared/toolbar.component/toolbar.component.ts
@@ -3,6 +3,8 @@ import {MatButton} from "@angular/material/button";
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatIcon} from '@angular/material/icon';
 import {RouterLink} from '@angular/router';
+import {AuthService} from '../../Auth/services/auth.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-toolbar',
@@ -12,10 +14,22 @@ import {RouterLink} from '@angular/router';
     MatIcon,
     MatToolbar,
     RouterLink,
+    NgIf,
   ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent {
+  userName = '';
+  businessName = '';
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    const user = this.authService.getCurrentUser();
+    if (user) {
+      this.userName = user.name;
+      this.businessName = user.businessName;
+    }
+  }
 }
